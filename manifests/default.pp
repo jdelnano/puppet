8a07f1fa0accd7e0c28e79f2d7690a461466dde8
Joseph Del Nano <josephdelnano@Josephs-MBP-2.fios-router.home>
2017-09-20 20:13:30 -0400

    Add extensions to configure apache correctly

--- manifests/default.pp
+++ manifests/default.pp
@@ -6,6 +6,19 @@ class { '::php':
   composer     => true,
   pear         => true,
   phpunit      => false,
+  extensions   => {
+            curl => {},
+	    pdo => {},
+	  mysql => {}
+            }
+}
+~>
+class { 'apache':
+  default_mods  => true,
+  default_vhost => false,
+  # This defaults to 'worker' on debian, setting to false 
+  # allows specifying our own.
+  mpm_module    =>  false,
 }
 
 class { '::mysql::server':
@@ -17,20 +30,13 @@ class { '::redis':
   bind => '0.0.0.0'
 }
 
-class { 'apache':
-  default_mods  => true,
-  default_vhost => false,
-  # This defaults to 'worker' on debian, setting to false 
-  # allows specifying our own.
-  mpm_module    =>  false,
-}
 
 include apache::mod::prefork
 include apache::mod::php
 
 apache::vhost { 'joechem.dev':
   servername => 'joechem.dev',
-  ip => '127.0.0.1',
-  port    => ['80','8080'],
+  ip => '0.0.0.0',
+  port    => '80',
   docroot => '/var/www/public'
 }
