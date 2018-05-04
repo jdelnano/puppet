f815499dfef6d6b5aeec92ec34f523585eb066df
Joseph Del Nano <josephdelnano@Josephs-MBP-2.fios-router.home>
2018-05-04 16:22:45 -0400

    Tweak mysql config

--- modules/roles/manifests/joechem.pp
+++ modules/roles/manifests/joechem.pp
@@ -97,15 +97,12 @@ class roles::joechem {
     class { '::mysql::server':
       root_password          => 'secret',
       remove_default_accounts => true,
-      override_options => {
-        mysqld => { bind-address => '127.0.0.1'} #Allow remote connections
-      },
     }
 
     mysql::db { 'joechem_master':
       user => 'joechem',
       password => 'secret',
-      host => '127.0.0.1',
+      host => '%',
       grant => ['SELECT', 'UPDATE', 'CREATE', 'ALTER', 'DELETE']
     }
  }
